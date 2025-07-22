8bbe35696c69d07ef7f1be28eab26b67cef5051d
Mohimem <sulimangzllal@alMacBook-Pro-mohimen-tantoush.local>
2025-07-23 01:40:46 +0200

    start working on login page

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -2,6 +2,8 @@
 <!-- make an example for login page here with modern style -->
 <div>
     <h1 style="color: white;">Hello Github 😎 I am mohimen Work</h1>
+    <!-- login page -->
+     
 </div>
   
  
